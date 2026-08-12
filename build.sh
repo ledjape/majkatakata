7c30d9a9817824cd9f5cfa6bbbfb7c5471192c0e
pegja galikj <pejahs@gmail.com>
2026-08-12 12:31:05 +0200

    Initialize production superuser credentials ledjape in build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+u, created = User.objects.get_or_create(username='ledjape', defaults={'email': 'pejahs@gmail.com', 'is_staff': True, 'is_superuser': True})
+u.set_password('K9#mQ2!vL8')
+u.is_staff = True
+u.is_superuser = True
+u.is_active = True
+u.save()
+print('Superuser ledjape initialized for production')
+"
