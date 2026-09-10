cbe81942baf44d9c19234c0a133473f4a3109296
pegja galikj <pejahs@gmail.com>
2026-09-10 15:29:37 +0200

    Switch Gmail SMTP to SSL port 465 with 5s timeout to prevent worker hang

--- config/settings.py
+++ config/settings.py
@@ -89,12 +89,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_TIMEOUT = 5
 EMAIL_HOST_USER = 'pejahs@gmail.com'
 EMAIL_HOST_PASSWORD = 'avfqhlsfwiiuufpi'
 DEFAULT_FROM_EMAIL = 'majkatakata <pejahs@gmail.com>'
 NOTIFICATION_RECIPIENTS = ['delikates@gmail.com', 'pejahs@gmail.com']
+
 
 
 
